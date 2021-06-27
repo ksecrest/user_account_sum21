74c8bf7073354c0f091aaee5a0221880a2a4915f
Katie Secrest <katie.secrest@gmail.com>
2021-06-26 23:52:17 -0600

    setup front end, index.js

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+     
+    validates :username, :membership, presence: true
+    validates :username, uniqueness: true
+    validates :membership, inclusion: {
+        in: %w( Bronze Silver Gold Platinum)
+    }
+
+end
